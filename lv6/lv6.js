68d66035a56ee19bb16f431db0e5a72efa0c35a9
Arisa174 <g1872042@tcu.ac.jp>
2021-05-19 22:08:50 +0900

    lv6終了

---
+++ lv6/lv6.js
@@ -0,0 +1,21 @@
+$(function(){
+
+    if ($('.js-scroll-trigger').length){
+        scrollAnimation();
+    }
+
+    function scrollAnimation(){
+        $(window).scroll(function(){
+            $(".js-scroll-trigger").each(function(){
+                let position = $(this).offset().top,
+                scroll = $(window).scrollTop(),
+                windowHeight = $(window).height();
+
+                if (scroll > position - windowHeight + 200){
+                    $(this).addClass('is-active');
+                }
+            });
+        });
+    }
+    $(window).trigger('scroll');
+});
